21bd25c19fc4cd51cbdc323675bdf6fcc0d371e0
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-21 09:27:07 -0700

    Refactor to remove filesystem during create

--- test/lib/loadEntries.js
+++ test/lib/loadEntries.js
@@ -1,14 +1,8 @@
-var path = require('path');
-var fs = require('fs');
+var fs = require('graceful-fs');
 var inherits = require('inherits');
-var assign = require('object-assign');
 var find = require('lodash.find');
-var Iterator = require('fs-iterator');
-var normalizeStats = require('normalize-stats');
-var toStats = require('big-int-stats/lib/toStats');
 
 var BaseIterator = require('../..');
-var entryToType = require('./entryToType');
 
 var DirectoryEntry = BaseIterator.DirectoryEntry;
 function FileEntry(attributes, contents) {
@@ -17,56 +11,53 @@ function FileEntry(attributes, contents) {
 }
 inherits(FileEntry, BaseIterator.FileEntry);
 FileEntry.prototype._writeFile = function _writeFile(fullPath, options, callback) {
+  // eslint-disable-next-line node/no-deprecated-api
   fs.writeFile(fullPath, this.contents, callback);
 };
 var LinkEntry = BaseIterator.LinkEntry;
 var SymbolicLinkEntry = BaseIterator.SymbolicLinkEntry;
 
-module.exports = function loadEntries(directory, callback) {
-  var basename = path.basename(directory);
-  var results = [];
-  results.push(new DirectoryEntry(assign({ path: basename }, fs.statSync(directory))));
+var constants = require('./constants');
+var CONTENTS = constants.CONTENTS;
+var STRUCTURE = {
+  'data/fixture.js': CONTENTS,
+  'data/symlink1': '~data/fixture.js',
+  'data/link1': ':data/fixture.js',
+  'data/dir1/fixture.js': CONTENTS,
+  'data/symlink2': '~data/dir1/fixture.js',
+  'data/link2': ':data/dir1/fixture.js',
+  'data/dir1/symlink1': '~data/dir1/fixture.js',
+  'data/dir1/link1': ':data/dir1/fixture.js',
+  'data/dir1/dir2/symlink1': '~data/dir1/fixture.js',
+  'data/dir1/dir2/link1': ':data/dir1/fixture.js',
+  'data/dir3/symlink1': '~data/dir1/fixture.js',
+  'data/dir3/link1': ':data/dir1/fixture.js',
+};
+var DMODE = parseInt(333, 8);
+var FMODE = parseInt(666, 8);
 
+function addDirectories(relativePath, entries) {
+  var parts = relativePath.split('/');
+  for (var index = 0; index < parts.length - 1; index++) {
+    var directoryPath = parts.slice(0, index + 1).join('/');
+
+    if (
+      !find(entries, function (entry) {
+        return entry instanceof DirectoryEntry && entry.path === directoryPath;
+      })
+    )
+      entries.push(new DirectoryEntry({ path: directoryPath, mode: DMODE, mtime: new Date() }));
+  }
+}
+
+module.exports = function loadEntries() {
   var entries = [];
-  new Iterator(directory, { alwaysStat: true, lstat: true }).forEach(
-    function (entry) {
-      entry.stats = toStats(normalizeStats(entry.stats));
-      entries.push(entry);
-    },
-    function (err) {
-      for (var index = 0; index < entries.length; index++) {
-        var entry = entries[index];
-        var type = entryToType(entry);
-        var relativePath = path.join(basename, entry.path);
-        var relativeDenormalizedPath = relativePath.split(path.sep).join('/');
-        var fullPath = path.join(directory, entry.path);
-
-        switch (type) {
-          case 'directory':
-            results.push(new DirectoryEntry(assign({ path: relativeDenormalizedPath }, entry.stats)));
-            break;
-          case 'file':
-            results.push(new FileEntry(assign({ path: relativeDenormalizedPath }, entry.stats), fs.readFileSync(fullPath)));
-            break;
-          case 'link':
-            var fileEntry = find(entries, function (x) {
-              return x.stats.ino === entry.stats.ino && x.basename.indexOf('link') !== 0;
-            });
-            var linkpath = path.join(basename, fileEntry.path);
-            var linkDenormalizedPath = linkpath.split(path.sep).join('/');
-            results.push(new LinkEntry(assign({ path: relativeDenormalizedPath, linkpath: linkDenormalizedPath }, entry.stats)));
-            break;
-          case 'symlink':
-            var linkFullPath = fs.realpathSync(fullPath);
-            // eslint-disable-next-line no-redeclare
-            var linkpath = path.relative(path.dirname(fullPath), linkFullPath);
-            // eslint-disable-next-line no-redeclare
-            var linkDenormalizedPath = linkpath.split(path.sep).join('/');
-            results.push(new SymbolicLinkEntry(assign({ path: relativeDenormalizedPath, linkpath: linkDenormalizedPath }, entry.stats)));
-            break;
-        }
-      }
-      err ? callback(err) : callback(null, results);
-    }
-  );
+  for (var relativePath in STRUCTURE) {
+    var contents = STRUCTURE[relativePath];
+    addDirectories(relativePath, entries);
+    if (contents[0] === ':') entries.push(new LinkEntry({ path: relativePath, linkpath: contents.slice(1), mode: FMODE, mtime: new Date() }));
+    else if (contents[0] === '~') entries.push(new SymbolicLinkEntry({ path: relativePath, linkpath: contents.slice(1), mode: FMODE, mtime: new Date() }));
+    else entries.push(new FileEntry({ path: relativePath, mode: FMODE, mtime: new Date() }, contents));
+  }
+  return entries;
 };
