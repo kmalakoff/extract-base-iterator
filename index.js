1e5e84fccdeff7f752a1fbec2d3ab13156f50ac0
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-28 23:19:54 -0700

    Migrated to just-extend

--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports = require('stack-base-iterator');
-module.exports.DirectoryEntry = require('./lib/DirectoryEntry');
-module.exports.FileEntry = require('./lib/FileEntry');
-module.exports.LinkEntry = require('./lib/LinkEntry');
-module.exports.SymbolicLinkEntry = require('./lib/SymbolicLinkEntry');
