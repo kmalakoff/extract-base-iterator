9a27f71cfa9907b76714b13f82636a3141a66a23
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 14:14:00 -0800

    Remove pinkie-promise dependency and update test environment

--- test/unit/asyncAwait.test.cjs
+++ test/unit/asyncAwait.test.cjs
@@ -41,14 +41,18 @@ async function extractForEach(iterator, dest, options) {
 
 describe('asyncAwait', () => {
   if (typeof Symbol === 'undefined' || !Symbol.asyncIterator) return;
-  let globalPromise;
-  before(() => {
-    globalPromise = global.Promise;
-    global.Promise = require('pinkie-promise');
-  });
-  after(() => {
-    global.Promise = globalPromise;
-  });
+  (() => {
+    // patch and restore promise
+    const root = typeof global !== 'undefined' ? global : window;
+    let rootPromise;
+    before(() => {
+      rootPromise = root.Promise;
+      root.Promise = require('pinkie-promise');
+    });
+    after(() => {
+      root.Promise = rootPromise;
+    });
+  })();
 
   const entries = loadEntries();
   beforeEach((callback) => {
