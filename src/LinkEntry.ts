db3e881f4b1ec1aaf48d1c1e0b44e911bcdc167b
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 05:51:00 -0700

    Upgrade to mandatory typescript extensions

--- src/LinkEntry.ts
+++ src/LinkEntry.ts
@@ -13,50 +13,56 @@ import validateAttributes from './validateAttributes.js';
 
 const MANDATORY_ATTRIBUTES = ['mode', 'mtime', 'path', 'linkpath'];
 
-export default function LinkEntry(attributes, _type) {
-  validateAttributes(attributes, MANDATORY_ATTRIBUTES);
-  objectAssign(this, attributes);
-  if (this.basename === undefined) this.basename = path.basename(this.path);
-  if (this.type === undefined) this.type = 'link';
-}
+export default class LinkEntry {
+  path: string;
+  basename: string;
+  type: string;
+  linkpath: string;
 
-LinkEntry.prototype.create = function create(dest, options, callback) {
-  if (typeof options === 'function') {
-    callback = options;
-    options = null;
+  constructor(attributes, _type) {
+    validateAttributes(attributes, MANDATORY_ATTRIBUTES);
+    objectAssign(this, attributes);
+    if (this.basename === undefined) this.basename = path.basename(this.path);
+    if (this.type === undefined) this.type = 'link';
   }
 
-  const self = this;
-  if (typeof callback === 'function') {
-    options = options || {};
-    try {
-      const normalizedPath = path.normalize(self.path);
-      const fullPath = path.join(dest, stripPath(normalizedPath, options));
-      const normalizedLinkpath = path.normalize(self.linkpath);
-      const linkFullPath = path.join(dest, stripPath(normalizedLinkpath, options));
-
-      const queue = new Queue(1);
-      if (options.force) {
-        queue.defer((callback) => {
-          rimraf2(fullPath, { disableGlob: true }, (err) => {
-            err && err.code !== 'ENOENT' ? callback(err) : callback();
-          });
-        });
-      }
-      queue.defer(mkdirp.bind(null, path.dirname(fullPath)));
-      queue.defer(fs.link.bind(fs, linkFullPath, fullPath));
-      queue.defer(chmod.bind(null, fullPath, self, options));
-      queue.defer(chown.bind(null, fullPath, self, options));
-      queue.defer(utimes.bind(null, fullPath, self, options));
-      return queue.await(callback);
-    } catch (err) {
-      return callback(err);
+  create(dest, options, callback) {
+    if (typeof options === 'function') {
+      callback = options;
+      options = null;
     }
+
+    if (typeof callback === 'function') {
+      options = options || {};
+      try {
+        const normalizedPath = path.normalize(this.path);
+        const fullPath = path.join(dest, stripPath(normalizedPath, options));
+        const normalizedLinkpath = path.normalize(this.linkpath);
+        const linkFullPath = path.join(dest, stripPath(normalizedLinkpath, options));
+
+        const queue = new Queue(1);
+        if (options.force) {
+          queue.defer((callback) => {
+            rimraf2(fullPath, { disableGlob: true }, (err) => {
+              err && err.code !== 'ENOENT' ? callback(err) : callback();
+            });
+          });
+        }
+        queue.defer(mkdirp.bind(null, path.dirname(fullPath)));
+        queue.defer(fs.link.bind(fs, linkFullPath, fullPath));
+        queue.defer(chmod.bind(null, fullPath, this, options));
+        queue.defer(chown.bind(null, fullPath, this, options));
+        queue.defer(utimes.bind(null, fullPath, this, options));
+        return queue.await(callback);
+      } catch (err) {
+        return callback(err);
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      this.create(dest, options, (err, done) => (err ? reject(err) : resolve(done)));
+    });
   }
 
-  return new Promise(function createPromise(resolve, reject) {
-    self.create(dest, options, (err, done) => (err ? reject(err) : resolve(done)));
-  });
-};
-
-LinkEntry.prototype.destroy = function destroy() {};
+  destroy() {}
+}
