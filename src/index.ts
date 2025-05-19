997f220ac01ec82832b1e75a05453f24e0a5f9f3
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-18 18:00:27 -0700

    Upgrade to mandatory typescript extensions

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import StackBaseIterator from 'stack-base-iterator';
+
+export default StackBaseIterator;
+export { default as DirectoryEntry } from './DirectoryEntry.js';
+export { default as FileEntry } from './FileEntry.js';
+export { default as LinkEntry } from './LinkEntry.js';
+export { default as SymbolicLinkEntry } from './SymbolicLinkEntry.js';
