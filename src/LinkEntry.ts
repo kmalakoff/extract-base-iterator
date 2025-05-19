16a5f747bf066d15f185e567f7a45516a2300ae3
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 06:23:44 -0700

    Add entry types

--- src/LinkEntry.ts
+++ src/LinkEntry.ts
@@ -12,6 +12,7 @@ import stripPath from './stripPath.js';
 import validateAttributes from './validateAttributes.js';
 
 const MANDATORY_ATTRIBUTES = ['mode', 'mtime', 'path', 'linkpath'];
+import type { LinkAttributes } from './types.js';
 
 export default class LinkEntry {
   path: string;
@@ -19,7 +20,7 @@ export default class LinkEntry {
   type: string;
   linkpath: string;
 
-  constructor(attributes) {
+  constructor(attributes: LinkAttributes) {
     validateAttributes(attributes, MANDATORY_ATTRIBUTES);
     objectAssign(this, attributes);
     if (this.basename === undefined) this.basename = path.basename(this.path);
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { default as DirectoryEntry } from './DirectoryEntry.js';
 export { default as FileEntry } from './FileEntry.js';
 export { default as LinkEntry } from './LinkEntry.js';
 export { default as SymbolicLinkEntry } from './SymbolicLinkEntry.js';
+export * from './types.js';
