dd6ce58412d0b1ccd62b62038e1588a89ff3b7a9
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 05:51:00 -0700

    Upgrade to mandatory typescript extensions

--- src/DirectoryEntry.ts
+++ src/DirectoryEntry.ts
@@ -11,41 +11,46 @@ import validateAttributes from './validateAttributes.js';
 
 const MANDATORY_ATTRIBUTES = ['mode', 'mtime', 'path'];
 
-export default function DirectoryEntry(attributes) {
-  validateAttributes(attributes, MANDATORY_ATTRIBUTES);
-  objectAssign(this, attributes);
-  if (this.type === undefined) this.type = 'directory';
-  if (this.basename === undefined) this.basename = path.basename(this.path);
-}
+export default class DirectoryEntry {
+  path: string;
+  basename: string;
+  type: string;
 
-DirectoryEntry.prototype.create = function create(dest, options, callback) {
-  if (typeof options === 'function') {
-    callback = options;
-    options = null;
+  constructor(attributes) {
+    validateAttributes(attributes, MANDATORY_ATTRIBUTES);
+    objectAssign(this, attributes);
+    if (this.type === undefined) this.type = 'directory';
+    if (this.basename === undefined) this.basename = path.basename(this.path);
   }
 
-  const self = this;
-  if (typeof callback === 'function') {
-    options = options || {};
-    try {
-      const normalizedPath = path.normalize(self.path);
-      const fullPath = path.join(dest, stripPath(normalizedPath, options));
-
-      // do not check for the existence of the directory but allow out-of-order calling
-      const queue = new Queue(1);
-      queue.defer(mkdirp.bind(null, fullPath));
-      queue.defer(chmod.bind(null, fullPath, self, options));
-      queue.defer(chown.bind(null, fullPath, self, options));
-      queue.defer(utimes.bind(null, fullPath, self, options));
-      return queue.await(callback);
-    } catch (err) {
-      return callback(err);
+  create(dest, options, callback) {
+    if (typeof options === 'function') {
+      callback = options;
+      options = null;
     }
+
+    if (typeof callback === 'function') {
+      options = options || {};
+      try {
+        const normalizedPath = path.normalize(this.path);
+        const fullPath = path.join(dest, stripPath(normalizedPath, options));
+
+        // do not check for the existence of the directory but allow out-of-order calling
+        const queue = new Queue(1);
+        queue.defer(mkdirp.bind(null, fullPath));
+        queue.defer(chmod.bind(null, fullPath, this, options));
+        queue.defer(chown.bind(null, fullPath, this, options));
+        queue.defer(utimes.bind(null, fullPath, this, options));
+        return queue.await(callback);
+      } catch (err) {
+        return callback(err);
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      this.create(dest, options, (err, done) => (err ? reject(err) : resolve(done)));
+    });
   }
 
-  return new Promise(function createPromise(resolve, reject) {
-    self.create(dest, options, (err, done) => (err ? reject(err) : resolve(done)));
-  });
-};
-
-DirectoryEntry.prototype.destroy = function destroy() {};
+  destroy() {}
+}
