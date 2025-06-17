abfa06b6442cc8a39fd986201c745150b1e41910
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-16 19:28:15 -0700

    Update EntriesIterator to return value object for stack-base-iterator v2

--- test/lib/EntriesIterator.cjs
+++ test/lib/EntriesIterator.cjs
@@ -7,11 +7,11 @@ function EntryIterator(entries, options) {
   self.entries = entries.slice();
 
   function next(iterator, callback) {
-    if (iterator.done || !self.entries.length) return callback(null, null);
+    if (iterator.done || !self.entries.length) return callback();
 
     // keep going
     iterator.push(next);
-    callback(null, self.entries.shift());
+    callback(null, { done: false, value: self.entries.shift() });
   }
   self.push(next);
 }
