1fbe4679626eb43e9d997ba9307860db9b634dcf
Kevin Malakoff <kmalakoff@gmail.com>
2025-11-30 17:03:19 -0800

    Added shared utitiies for all of the iterators

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,7 @@ export { default as DirectoryEntry } from './DirectoryEntry.ts';
 export { default as FileEntry } from './FileEntry.ts';
 export { default as LinkEntry } from './LinkEntry.ts';
 export { default as SymbolicLinkEntry } from './SymbolicLinkEntry.ts';
+// Shared utilities for iterator libraries
+export * from './shared/index.ts';
 export * from './types.ts';
 export { default as waitForAccess } from './waitForAccess.ts';
