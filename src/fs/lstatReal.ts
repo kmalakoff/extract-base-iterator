c24a63a5a675f1e18e261b23821e31dc5fa034c3
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 16:49:31 -0800

    Upgrade to void

--- src/fs/lstatReal.ts
+++ src/fs/lstatReal.ts
@@ -2,7 +2,7 @@ import fs, { type BigIntStats, type Stats } from 'fs';
 
 export type Callback = (err: NodeJS.ErrnoException | null, stats?: Stats | BigIntStats) => void;
 
-export default function lstatReal(path: string, callback: Callback): undefined {
+export default function lstatReal(path: string, callback: Callback): void {
   fs.realpath(path, (err, realpath) => {
     err ? callback(err) : fs.lstat(realpath, callback);
   });
--- src/fs/utimes.ts
+++ src/fs/utimes.ts
@@ -4,7 +4,7 @@ import type { NoParamCallback } from 'fs';
 import fs from 'graceful-fs';
 import type { AbstractEntry, ExtractOptions } from '../types.ts';
 
-export default function utimes(fullPath: string, entry: AbstractEntry, options: ExtractOptions, callback: NoParamCallback): undefined {
+export default function utimes(fullPath: string, entry: AbstractEntry, options: ExtractOptions, callback: NoParamCallback): void {
   const now = options.now || new Date();
   fs.utimes(fullPath, now, new Date(entry.mtime), callback);
 }
