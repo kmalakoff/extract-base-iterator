72539533ef6a5ccf6e75c992b23ff04c83555800
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-19 10:31:40 -0800

    Exported streams

--- src/shared/compat.ts
+++ src/shared/compat.ts
@@ -328,3 +328,14 @@ export function objectAssign<T, U>(target: T, source: U): T & U {
   }
   return target as T & U;
 }
+
+/**
+ * Stream compatibility - Transform class
+ * - Uses native stream.Transform on Node 0.10+
+ * - Falls back to readable-stream for Node 0.8
+ */
+const major = +process.versions.node.split('.')[0];
+export const Readable: typeof import('stream').Readable = major > 0 ? _require('stream').Readable : _require('readable-stream').Readable;
+export const Writable: typeof import('stream').Writable = major > 0 ? _require('stream').Writable : _require('readable-stream').Writable;
+export const Transform: typeof import('stream').Transform = major > 0 ? _require('stream').Transform : _require('readable-stream').Transform;
+export const PassThrough: typeof import('stream').PassThrough = major > 0 ? _require('stream').PassThrough : _require('readable-stream').PassThrough;
